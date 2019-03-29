dd69221d7428175242cca15e41bc7aa1dc0e3bbc
Simon Cain <rudenik@gmail.com>
2019-03-29 13:52:45 -0400

    added some more logic

--- assets/app.js
+++ assets/app.js
@@ -16,16 +16,33 @@ function percent(){
     var minutesInWorkDay = 60*8.5
     var timeSinceWorkStarted = (now.getMinutes()+(now.getHours()*60))-(7.5*60);
     var percentage = timeSinceWorkStarted / minutesInWorkDay;
-    console.log("Now in minutes: ", now.getMinutes());
-    console.log("Minutes in work day: ", minutesInWorkDay);
-    console.log("MinuteSinceWork Started: ", timeSinceWorkStarted);
+    // console.log("Now in minutes: ", now.getMinutes());
+    // console.log("Minutes in work day: ", minutesInWorkDay);
+    // console.log("MinuteSinceWork Started: ", timeSinceWorkStarted);
     console.log("Percentage: ", percentage*100);
 
     var percentSpan = $("#percentage")
-    if((percentage*100) > 50){
-        percentSpan.text("Almost Time to go home");
-    }else if((percentage*100) < 50){
-        percentSpan.text("Almost Time for lunch");
+    if((percentage*100) > 100){
+        percentSpan.text("It's over, Go Home!");
+    }else if((percentage*100) > 80){
+        percentSpan.text("Breathe, it's almost time to go home");
+    }else if((percentage*100) > 59){
+        percentSpan.text("You got this, keep going");
+    }   
+    else if((percentage*100) > 47){
+        percentSpan.text("Stop what you're doing, get lunch");
+    }   
+    else if((percentage*100) > 35){
+        percentSpan.text("Almost time for lunch");
+    }   
+    else if((percentage*100) > 10){
+        percentSpan.text("Just think, this might be the most productive you'll be all day");
+    }   
+    else if((percentage*100) < 10){
+        percentSpan.text("You shouldn't be checking the percentage yet, you haven't been here long enough");
+    }else if ((percentage*100) < 0){
+      percentSpan.text("You got to work too early. Take a nap");
+
     }   
     
     var elem = document.getElementById("myBar");
